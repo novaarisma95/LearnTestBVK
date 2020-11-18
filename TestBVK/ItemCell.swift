2e5d254dcf1839898c70a7947efdcfc863a6687c
Nova Arisma <novaarisma95@gmail.com>
2020-11-18 10:32:54 +0700

    workDay1

---
+++ TestBVK/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  TestBVK
+//
+//  Created by Nova Arisma on 11/18/20.
+//  Copyright © 2020 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
