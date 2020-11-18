46e5bb6d9f0acaf81288724942217386581e9572
Nova Arisma <novaarisma95@gmail.com>
2020-11-18 09:03:02 +0700

    Initial Commit

---
+++ TestBVK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBVK
+//
+//  Created by Nova Arisma on 11/18/20.
+//  Copyright © 2020 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
